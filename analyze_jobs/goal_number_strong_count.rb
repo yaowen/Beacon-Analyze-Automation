edf5cad84b86c67abc070ba50cb64857ad313127
Yaowen Zheng <yaowen.zheng@hulu.com>
2012-06-28 13:19:01 +0800

    add strong analyze which will only consider those session start at frontporch landing

---
+++ analyze_jobs/goal_number_strong_count.rb
@@ -0,0 +1,86 @@
+require './analyze_job'
+require 'date'
+class GoalNumberStrongCountAnalyzeJob < AnalyzeJob
+  
+  def initialize
+    @visits = {}
+    @goal_count = {}
+    @output_filename = "goal_number"
+    @result = ""
+  end
+  # ==> methods derivation Has to implement
+  def analyze_session session
+    visit_time = DateTime.parse(session[0]["visit_time"])
+    mark_landing = false
+    mark_conversion = false
+
+    unless during?(
+      session[0]["visit_time"],
+      "2012-05-17 22:00:00 +0900",
+      "2012-05-22 11:00:00 +0900")
+      return
+    end
+
+    version = ""
+    analyze session do |action|
+      if !mark_landing and action["_type"] == "page_load"
+        unless front_porch? action
+          return
+        end
+        mark_landing = true
+        version = extract_version action["pageurl"]
+      end
+      
+      if conversion? action
+        mark_conversion = true
+      end
+    end
+    @visits[version] ||= {}
+    @visits[version][visit_time.wday] ||= 0 
+    @visits[version][visit_time.wday] += 1
+
+    if mark_conversion
+      @goal_count[version] ||= {}
+      @goal_count[version][visit_time.wday] ||= 0
+      @goal_count[version][visit_time.wday] += 1
+    end
+  end
+
+  def output_format
+  end
+
+  def output_csv_format
+    @csv = CSV.generate do |csv_data|
+      csv_data << [
+        "Day",
+        "Version",
+        "Visits",
+        "Conversion",
+        "Conversion Rate"
+      ]
+      @visits.each do |version, visit_day_data|
+        puts version
+        visit_total_count = 0
+        conversion = 0
+        visit_day_data.each do |weekday, visit_count|
+          @goal_count[version] ||= {}
+          @goal_count[version][weekday] ||= 0
+          visit_total_count += visit_count
+          conversion += @goal_count[version][weekday]
+          conversion_rate = conversion * 1.0 / visit_total_count
+          puts "con and visit: " + conversion.to_s + " " + visit_total_count.to_s
+
+          csv_data << [
+            weekday.to_s,
+            version.to_s,
+            visit_total_count.to_s,
+            conversion.to_s,
+            conversion_rate.to_s
+          ]
+        end
+      end
+    end
+  end
+end
+
+
---
+++ analyze_jobs/signup_funnel_strong_analyze.rb
@@ -0,0 +1,138 @@
+require './analyze_job'
+require 'set'
+class SignupFunnelStrongAnalyzeJob < AnalyzeJob
+  
+  def initialize
+    super
+    @states_counter = Hash.new
+    @output_filename = "signup_funnel"
+    @result = ""
+    @total = 0
+  end
+  # ==> methods derivation Has to implement
+
+  def pick_when action, type, field, target_value
+    if action["_type"] == type and action[field] =~ target_value
+      
+    end
+  end
+
+  def add_one counter, field
+    counter[field] ||= 0
+    counter[field] += 1
+  end
+
+  def formalize
+    marks = ["frontporch", "signup_start", "email", "step1", "card", "conversion"]
+    @states_counter.each do |key, state_counter|
+      marks.each do |mark|
+        state_counter[mark] ||= 0
+      end
+    end
+  end
+
+  def analyze_session session
+    marks = Set.new
+    unless during?(
+      session[0]["visit_time"], 
+      "2012-05-17 22:00:00 +0900", 
+      "2012-05-22 11:00:00 +0900")
+      return
+    end
+    mark_landing = false
+    version = ""
+    analyze session do |action|
+      if !mark_landing && action["_type"] == "page_load"
+        unless front_porch? action
+          return
+        end
+        version = extract_version action["pageurl"]
+        mark_landing = true
+      end
+      if action["_type"] == "signup_action"
+        if action["field"] == "email"
+          marks.add "email"
+        elsif action["field"] == "continue_s2"
+          marks.add "step1"
+        elsif action["field"] == "card"
+          marks.add "card"
+        end
+      elsif action["_type"] == "page_load"
+        if front_porch? action
+          marks.add "frontporch"
+        elsif conversion? action
+          marks.add "conversion"
+        elsif signup_start? action
+          marks.add "signup_start"
+        end
+      end
+    end
+
+    unless mark_landing
+      return
+    end
+
+    @states_counter[version] ||= {}
+    marks.each do |mark|
+      add_one @states_counter[version], mark
+    end
+    formalize
+  end
+
+  def output_format
+
+    @states_counter.each do |key, state_counter|
+      @result += "version: #{key}\n"
+      @result += "Landing: #{state_counter["frontporch"]}\n"
+      @result += "Signup Start: #{state_counter["signup_start"]}\n"
+      @result += "Input Email: #{state_counter["email"]}\n"
+      @result += "Step1 Complete: #{state_counter["step1"]}\n"
+      @result += "Input CC: #{state_counter["card"]}\n"
+      @result += "Conversion: #{state_counter["conversion"]}\n"
+    end
+  end
+
+  def output_csv_format
+    @csv = CSV.generate do |csv_data|
+      csv_data << [
+        "Version",
+        "Landing", 
+        "Signup Start", 
+        "Input Email", 
+        "Step1 Complete",
+        "Input CC", 
+        "Conversion"]
+      @states_counter.each do |key, state_counter|
+        # ==> Head Part
+
+        # ==> Value Part
+        csv_data << [
+          key,
+          state_counter["frontporch"],
+          state_counter["signup_start"],
+          state_counter["email"],
+          state_counter["step1"],
+          state_counter["card"],
+          state_counter["conversion"]
+        ]
+      end
+    end
+  end
+
+  def output_email_format
+    @email_report ||= ""
+    @states_counter.each do |key, state_counter|
+      landing_count = state_counter["frontporch"] 
+      signup_start_rate = state_counter["signup_start"] * 100.0 / landing_count
+      email_input_rate = state_counter["email"] * 100.0 / landing_count
+      input_credit_card_rate = (state_counter["card"] ) * 100.0 / landing_count
+      conversion_rate = state_counter["conversion"] * 100.0 / landing_count
+      content = <<-HTML
+        <b> version: #{key}, landing: #{state_counter["frontporch"]}, signup_start: #{state_counter["signup_start"]}(#{signup_start_rate}%), email: #{state_counter["email"]}(#{email_input_rate}%), input_credit_card: #{state_counter["card"]}(#{input_credit_card_rate}%), final_conversion: #{state_counter["conversion"]}(#{conversion_rate}%) </b>
+      HTML
+      @email_report += content
+    end
+  end
+end
+
+
